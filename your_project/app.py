7cc99d50f7ebafa2a5cc8faab218d4d623119057
Niklas Rosenstein <NiklasRosenstein@users.noreply.github.com>
2016-05-28 15:05:53 +0200

    Move db binding and creation of DEPLOY_DIR to app.run()

--- your_project/app.py
+++ your_project/app.py
@@ -1,3 +1,4 @@
+import os
 import flask
 from . import config
 
@@ -6,4 +7,8 @@ app = flask.Flask(__name__.split('.')[0])
 from . import models, views
 
 def run(*args, **kwargs):
+  if not os.path.exists(config.DEPLOY_DIR):
+    os.makedirs(config.DEPLOY_DIR)
+  models.db.bind(config.DB_PROVIDER, **config.DB_CONFIG)
+  models.db.generate_mapping(create_tables=True)
   return app.run(*args, **kwargs)
